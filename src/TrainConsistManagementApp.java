78a2f75bc189be33ed1b641d4596feaa5c411df3
hc1427-lab <hc1427@srmist.edu.in>
2026-03-18 14:50:42 +0530

    Base application setup

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args){
+        System.out.println("=====================================");
+        System.out.println("Train Consist Management App");
+        System.out.println("=====================================");
+        List<String> TrainConsist=new ArrayList<String>();
+        System.out.println("Train Initialized Successfully.....");
+
+    }
+}
